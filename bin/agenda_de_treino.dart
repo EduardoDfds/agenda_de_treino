b544a7fa7f88f4b2a1561931e18fa1060191b001
EduardoDfs <dueduedudu1>
2022-05-03 21:18:47 -0300

    firrst commit

---
+++ bin/agenda_de_treino.dart
@@ -0,0 +1,5 @@
+import 'package:agenda_de_treino/agenda_de_treino.dart' as agenda_de_treino;
+
+void main(List<String> arguments) {
+  print('Hello world: ${agenda_de_treino.calculate()}!');
+}
---
+++ test/agenda_de_treino_test.dart
@@ -0,0 +1,8 @@
+import 'package:agenda_de_treino/agenda_de_treino.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
